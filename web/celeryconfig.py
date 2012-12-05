e86b34320f084214f5fe79f5fb65f54c6f61908b
wanghao <wangh@opengoss.com>
2012-12-05 14:24:51 +0800

    fix

--- web/celeryconfig.py
+++ web/celeryconfig.py
@@ -18,6 +18,6 @@ from datetime import timedelta
 CELERYBEAT_SCHEDULE = {
     'runs-every-15-minites': {
         'task': 'webapp.read_files',
-        'schedule': timedelta(seconds=5)   #minutes=15
+        'schedule': timedelta(hours=1)   #minutes=15
     },
 }
